9edf04953ac445dc39795b6b31147f735be6279e
Andrea Piseri <andrea.piseri@gmail.com>
2022-01-08 20:33:37 +0100

    add procedure type parsing

--- grammar.js
+++ grammar.js
@@ -11,11 +11,8 @@ const
 
 
   raw_string_literal = seq('`', repeat(choice(/[^\\`]|\\./, newline)), '`'),
-  string_literal = seq('"', /([^\\"]|\\.)*/ , '"')
-
-
-
-
+  string_literal = seq('"', /([^\\"]|\\.)*/ , '"'),
+  as_list = (elem) => (seq(elem, repeat(seq(',', elem))))
 
 module.exports = grammar({
   name: 'odin',
@@ -41,14 +38,45 @@ module.exports = grammar({
     value_declare: $ => seq(
       field('name', $._identifier_list), ':', 
       choice(
-        field('type', $.identifier), 
+        field('type', $._type), 
         seq(optional(field('type', $.identifier)), '=', field('value', $._expression_list)),
         seq(optional(field('type', $.identifier)), ':', field('const', $._expression_list))
       )
     ),
 
+    single_value_declare: $ => seq(
+      field('name', $.identifier), ':', 
+      choice(
+        field('type', $._type), 
+        seq(optional(field('type', $.identifier)), '=', field('value', $._expression)),
+        seq(optional(field('type', $.identifier)), ':', field('const', $._expression))
+      )
+    ),
+
     _expression_list: $ => seq($._expression, repeat(seq(',', $._expression))),
-    _expression: $ => choice($._literal, $.proc_call, $.identifier),
+    _expression: $ => prec(2, choice($._literal, $.proc_call, $.identifier, $._type)),
+
+    proc_type: $ => seq('#type', $._proc_signature),
+
+    _proc_signature: $ => seq(
+      'proc', '(', 
+      repeat(field('arg', $._value_declare_list)),
+      ')', 
+      optional(seq(
+        '->', 
+        field('returns', choice(
+           $._type, 
+          seq('(', $._type_list ,')')
+        ))
+      ))
+    ),
+
+    _type_list: $ => as_list($._type),
+    _value_declare_list: $ => as_list($.single_value_declare),
+
+    type: $ => $._type,
+    _type: $ => prec(1, choice($.identifier, $.type_of, $.proc_type)),
+    type_of: $ => seq('type_of', '(', $._expression, ')'),
 
     _literal: $ => choice(
       $.string_literal, 
@@ -61,12 +89,20 @@ module.exports = grammar({
     identifier: $ => token(seq(letter, repeat(choice(letter, unicode_digit)))),
     _identifier_list: $ => seq($.identifier, repeat(seq(',', $.identifier))),
 
+
+    //
+    // literals
+    //
     string_literal: $ => string_literal,
     raw_string_literal: $ => raw_string_literal,
     int_literal: $ => token(decimals),
 
-    line_comment: $ => token(seq('//', /.*/)),
 
+
+    //
+    // comments
+    //
+    line_comment: $ => token(seq('//', /.*/)),
     multiline_comment: $ => seq('/*', $._mutlitline_comment_tail),
     _mutlitline_comment_tail: $ => seq( 
       repeat(choice(/[^/*]/, '\n')),  // scan up to the first *, / or \n
---
+++ test.odin
@@ -0,0 +1,19 @@
+package foo
+
+import foo "core:foo"
+import "core:slice"
+
+s := "hi\"  hello\" " 
+
+/* multiline 
+  /* nested /* and triply so */ */
+comment */
+
+v := `this is a multiline
+string literal
+`
+c : int : 100_000
+
+a, b := 4, 5
+
+a := function(4, 3, "hi")
