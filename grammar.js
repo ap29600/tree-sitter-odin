c4d34701bad4e7547a678799cf6b5a3520834fac
Andrea Piseri <andrea.piseri@gmail.com>
2022-02-02 18:21:44 +0100

    add support for `using` keyword

--- grammar.js
+++ grammar.js
@@ -1,18 +1,18 @@
 /*
  *  The MIT License (MIT)
- *  
+ *
  *  Copyright (c) 2022 Andrea Piseri
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -209,6 +209,7 @@ module.exports = grammar({
         $.for_statement,
         $.block_statement,
         seq(alias('defer', $.keyword), $._statement),
+        seq(alias('using', $.keyword), $.identifier),
         $.return_statement,
       )
     ),
@@ -417,11 +418,11 @@ module.exports = grammar({
       'proc',
       optional($._calling_convention),
       field('parameters', $.parameter_list),
-      field('result', optional(seq('->', choice($.parameter_list, $._type))))
+      field('result', optional(seq(alias('->', $.operator), choice($.parameter_list, $._type))))
     ),
 
     parameter_list: $ => seq(
-      '(', 
+      '(',
       commaSep(seq(
         choice(
           prec.dynamic(1, $.parameter_declaration),
@@ -432,8 +433,18 @@ module.exports = grammar({
     ),
 
     parameter_declaration: $ => seq(
-      field('name', seq(choice(commaSep1($.identifier), commaSep1(seq('$', $.identifier))), ':')),
-      field('type', seq($._type, optional(seq(alias('/', $.operator), $._type)))), 
+      field('name', seq(
+        commaSep1(seq(
+          optional(alias('using', $.keyword)),
+          optional(alias('$', $.operator)),
+          $.identifier,
+        )),
+        ':',
+      )),
+      field('type', seq(
+        $._type,
+        optional(seq(alias('/', $.operator), $._type))
+      )),
       optional(seq('=', field('value', $._expression))),
     ),
 
