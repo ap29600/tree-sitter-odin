004465be7c1b71b24393bb4d270f810a2ba3f552
Andrea Piseri <andrea.piseri@gmail.com>
2022-08-02 15:25:51 +0200

    add rudimentary `indents.scm`

---
+++ queries/indents.scm
@@ -0,0 +1,19 @@
+[
+ (block_statement)
+ (proc_literal)
+ (compound_literal)
+ (proc_call)
+ (assignment_statement)
+ (const_declaration)
+ (var_declaration)
+ (if_statement)
+ (for_statement)
+ (switch_statement)
+] @indent
+
+[
+ ")"
+ "]"
+ "}"
+] @indent_end @branch
+
